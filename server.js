717177b86d52a61e4485ac65cb9be50093993303
flavio <flaviomouracar7800@gmail.com>
2022-11-06 22:21:57 -0400

    Inserida a porta e função ao qual o servidor aguardará a conexão

--- server.js
+++ server.js
@@ -4,3 +4,9 @@ const app = express() //método construtor do codigo acima para instanciar uma a
 
 const http = require('http').Server(app)// retorna um modulo http que contem algumas classes que no caso esta sendo ultilizado o server que retornará uma conexão http
 const serverSocket = require('socket.io')(http)
+
+const PORTA = 8000
+
+http.listen(PORTA, function (){
+    console.log('Servidor iniciado. Abra o navegador em http://localhost:' + PORTA);
+})
