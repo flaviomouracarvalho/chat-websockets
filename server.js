20d936f981881db52f100af56bf9f7853a9aab3a
flavio <flaviomouracar7800@gmail.com>
2022-11-06 23:37:51 -0400

    Implementado recebimento de envio de mensagem do lado cliente.

--- server.js
+++ server.js
@@ -2,6 +2,8 @@
 const express = require('express') //função retornada pelo require
 const app = express() //método construtor do codigo acima para instanciar uma aplicação 
 
+app.use(express.static('public'))
+
 const http = require('http').Server(app)// retorna um modulo http que contem algumas classes que no caso esta sendo ultilizado o server que retornará uma conexão http
 const serverSocket = require('socket.io')(http)
 
@@ -9,4 +11,8 @@ const PORTA = 8000
 
 http.listen(PORTA, function (){
     console.log('Servidor iniciado. Abra o navegador em http://localhost:' + PORTA);
+})
+
+app.get('/', function(req, resp){
+    resp.sendFile(__dirname + '/index.html')
 })
